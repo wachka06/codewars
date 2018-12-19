3630b69937e3a36e14c5edbef2362e96c80b2a56
wachka06 <wachka06@gmail.com>
2018-12-18 21:31:14 -0500

    problems

--- 4_Multiplying_numbers_as_strings.js
+++ 4_Multiplying_numbers_as_strings.js
@@ -6,7 +6,7 @@ The arguments are passed as strings.
 The numbers may be way very large
 Answer should be returned as a string
 The returned "number" should not start with zeros e.g. 0123 is invalid
-Note: 100 randomly generated tests!
+Note: 100 randomly generated tests!    
 
 https://www.codewars.com/kata/55911ef14065454c75000062/train/javascript
 ***********************************************************************/
