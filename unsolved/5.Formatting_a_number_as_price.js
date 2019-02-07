9bacacfc70ee75c16502ac0b50ffee9ba0ccbc90
wachka06 <wachka06@gmail.com>
2019-02-06 21:18:59 -0500

    done

--- unsolved/5.Formatting_a_number_as_price.js
+++ unsolved/5.Formatting_a_number_as_price.js
@@ -34,6 +34,7 @@ var numberToPrice = function(number) {
   return result + '.' + numArr[1]
 }
 
+
 console.log(numberToPrice(1500.129)) //=>  '1,500.12'
 console.log(numberToPrice(-5)) //=>         '-5.00'
 console.log(numberToPrice(1000000.5)) //=>  '1,000,000.50'
