a22c3bd241d93ca7c3efe3f2b2170eb61ef404f0
wachka06 <wachka06@gmail.com>
2019-02-11 17:00:46 -0500

    done

--- almost/5.Regex_Password_Validation.js
+++ almost/5.Regex_Password_Validation.js
@@ -45,7 +45,6 @@ function validate(password) {
   return true
 }
 
-
 console.log(validate('djI38D55')) //=> 'djI38D55 - Expected true'
 console.log(!validate('a2.d412')) //=> 'a2.d412 - Expected false'
 console.log(!validate('JHD5FJ53')) //=> 'JHD5FJ53 - Expected false'
