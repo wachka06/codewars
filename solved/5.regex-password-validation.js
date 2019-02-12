5c8c41e45a8cc9ec635aae98f479389a75e0a487
wachka06 <wachka06@gmail.com>
2019-02-12 18:40:05 -0500

    solved

--- solved/5.regex-password-validation.js
+++ solved/5.regex-password-validation.js
@@ -47,12 +47,11 @@ function validate(password) {
 }
 
 
-
 console.log(validate('djI38D55')) //=> 'djI38D55 - Expected true'
-console.log(!validate('a2.d412')) //=> 'a2.d412 - Expected false'
-console.log(!validate('JHD5FJ53')) //=> 'JHD5FJ53 - Expected false'
-console.log(!validate('!fdjn345')) //=> '!fdjn345 - Expected false'
-console.log(!validate('jfkdfj3j')) //=> 'jfkdfj3j - Expected false'
-console.log(!validate('123')) //=> '123 - Expected false'
-console.log(!validate('abc')) //=> 'abc - Expected false'
+console.log(validate('a2.d412')) //=> 'a2.d412 - Expected false'
+console.log(validate('JHD5FJ53')) //=> 'JHD5FJ53 - Expected false'
+console.log(validate('!fdjn345')) //=> '!fdjn345 - Expected false'
+console.log(validate('jfkdfj3j')) //=> 'jfkdfj3j - Expected false'
+console.log(validate('123')) //=> '123 - Expected false'
+console.log(validate('abc')) //=> 'abc - Expected false'
 console.log(validate('Password123')) //=> 'Password123 - Expected true'
