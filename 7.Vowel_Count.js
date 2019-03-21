badd8d8b22c65af5eac6a81883b3c428ff494d83
wachka06 <wachka06@gmail.com>
2019-03-21 17:40:33 -0400

    add problem

---
+++ 7.Vowel_Count.js
@@ -0,0 +1,19 @@
+/***********************************************************************
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+
+The input string will only consist of lower case letters and/or spaces.
+
+https://www.codewars.com/kata/vowel-count/train/javascript
+***********************************************************************/
+
+function getCount(str) {
+  var vowelsCount = 0;
+
+  // enter your majic here
+
+  return vowelsCount;
+}
+
+console.log(getCount("abracadabra") //=> 5
