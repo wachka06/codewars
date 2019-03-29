b2ed8952c465b697da62c05fb62942e8b87a52b1
wachka06 <wachka06@gmail.com>
2019-03-29 14:14:47 -0400

    add problem

---
+++ 6.Unique_In_Order.rb
@@ -0,0 +1,19 @@
+##***********************************************************************
+## Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+## For example:
+
+## uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+## uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+## uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+## https://www.codewars.com/kata/unique-in-order/train/javascript
+##***********************************************************************
+
+def unique_in_order(iterable)
+
+end
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB')) ## => ['A', 'B', 'C', 'D', 'A', 'B']
+console.log(uniqueInOrder('ABBCcAD'))         ## => ['A', 'B', 'C', 'c', 'A', 'D']
+console.log(uniqueInOrder([1,2,2,3,3]))       ## => [1,2,3]
